7dd06df2ab3954adcea2564a83dd877bfb962ab0
roothorick <roothorick@gmail.com>
2017-06-18 15:32:37 -0500

    Clone and proxy classes for IVRCompositor and IVRRenderModels

---
+++ src/ivrcompositor_32.cpp
@@ -0,0 +1,300 @@
+#include <cstdio>
+
+// HACK: No idea where this should actually be defined
+#define nullptr 0x0
+#include "openvr.h"
+using namespace vr;
+
+#define THISCALL __attribute__((thiscall))
+
+class clone_IVRCompositor
+{
+public:
+	THISCALL virtual void SetTrackingSpace( ETrackingUniverseOrigin eOrigin ) = 0;
+	THISCALL virtual ETrackingUniverseOrigin GetTrackingSpace() = 0;
+	THISCALL virtual EVRCompositorError WaitGetPoses( VR_ARRAY_COUNT(unRenderPoseArrayCount) TrackedDevicePose_t* pRenderPoseArray, uint32_t unRenderPoseArrayCount,
+		VR_ARRAY_COUNT(unGamePoseArrayCount) TrackedDevicePose_t* pGamePoseArray, uint32_t unGamePoseArrayCount ) = 0;
+	THISCALL virtual EVRCompositorError GetLastPoses( VR_ARRAY_COUNT( unRenderPoseArrayCount ) TrackedDevicePose_t* pRenderPoseArray, uint32_t unRenderPoseArrayCount,
+		VR_ARRAY_COUNT( unGamePoseArrayCount ) TrackedDevicePose_t* pGamePoseArray, uint32_t unGamePoseArrayCount ) = 0;
+	THISCALL virtual EVRCompositorError GetLastPoseForTrackedDeviceIndex( TrackedDeviceIndex_t unDeviceIndex, TrackedDevicePose_t *pOutputPose, TrackedDevicePose_t *pOutputGamePose ) = 0;
+	THISCALL virtual EVRCompositorError Submit( EVREye eEye, const Texture_t *pTexture, const VRTextureBounds_t* pBounds = 0, EVRSubmitFlags nSubmitFlags = Submit_Default ) = 0;
+	THISCALL virtual void ClearLastSubmittedFrame() = 0;
+	THISCALL virtual void PostPresentHandoff() = 0;
+	THISCALL virtual bool GetFrameTiming( Compositor_FrameTiming *pTiming, uint32_t unFramesAgo = 0 ) = 0;
+	THISCALL virtual uint32_t GetFrameTimings( Compositor_FrameTiming *pTiming, uint32_t nFrames ) = 0;
+	THISCALL virtual float GetFrameTimeRemaining() = 0;
+	THISCALL virtual void GetCumulativeStats( Compositor_CumulativeStats *pStats, uint32_t nStatsSizeInBytes ) = 0;
+	THISCALL virtual void FadeToColor( float fSeconds, float fRed, float fGreen, float fBlue, float fAlpha, bool bBackground = false ) = 0;
+	THISCALL virtual void GetCurrentFadeColor( HmdColor_t* ret, bool bBackground = false ) = 0; // ERP hack
+	THISCALL virtual void FadeGrid( float fSeconds, bool bFadeIn ) = 0;
+	THISCALL virtual float GetCurrentGridAlpha() = 0;
+	THISCALL virtual EVRCompositorError SetSkyboxOverride( VR_ARRAY_COUNT( unTextureCount ) const Texture_t *pTextures, uint32_t unTextureCount ) = 0;
+	THISCALL virtual void ClearSkyboxOverride() = 0;
+	THISCALL virtual void CompositorBringToFront() = 0;
+	THISCALL virtual void CompositorGoToBack() = 0;
+	THISCALL virtual void CompositorQuit() = 0;
+	THISCALL virtual bool IsFullscreen() = 0;
+	THISCALL virtual uint32_t GetCurrentSceneFocusProcess() = 0;
+	THISCALL virtual uint32_t GetLastFrameRenderer() = 0;
+	THISCALL virtual bool CanRenderScene() = 0;
+	THISCALL virtual void ShowMirrorWindow() = 0;
+	THISCALL virtual void HideMirrorWindow() = 0;
+	THISCALL virtual bool IsMirrorWindowVisible() = 0;
+	THISCALL virtual void CompositorDumpImages() = 0;
+	THISCALL virtual bool ShouldAppRenderWithLowResources() = 0;
+	THISCALL virtual void ForceInterleavedReprojectionOn( bool bOverride ) = 0;
+	THISCALL virtual void ForceReconnectProcess() = 0;
+	THISCALL virtual void SuspendRendering( bool bSuspend ) = 0;
+	THISCALL virtual vr::EVRCompositorError GetMirrorTextureD3D11( vr::EVREye eEye, void *pD3D11DeviceOrResource, void **ppD3D11ShaderResourceView ) = 0;
+	THISCALL virtual void ReleaseMirrorTextureD3D11( void *pD3D11ShaderResourceView ) = 0;
+	THISCALL virtual vr::EVRCompositorError GetMirrorTextureGL( vr::EVREye eEye, vr::glUInt_t *pglTextureId, vr::glSharedTextureHandle_t *pglSharedTextureHandle ) = 0;
+	THISCALL virtual bool ReleaseSharedGLTexture( vr::glUInt_t glTextureId, vr::glSharedTextureHandle_t glSharedTextureHandle ) = 0;
+	THISCALL virtual void LockGLSharedTextureForAccess( vr::glSharedTextureHandle_t glSharedTextureHandle ) = 0;
+	THISCALL virtual void UnlockGLSharedTextureForAccess( vr::glSharedTextureHandle_t glSharedTextureHandle ) = 0;
+	THISCALL virtual uint32_t GetVulkanInstanceExtensionsRequired( VR_OUT_STRING() char *pchValue, uint32_t unBufferSize ) = 0;
+	THISCALL virtual uint32_t GetVulkanDeviceExtensionsRequired( VkPhysicalDevice_T *pPhysicalDevice, VR_OUT_STRING() char *pchValue, uint32_t unBufferSize ) = 0;
+};
+
+class proxy_IVRCompositor : public clone_IVRCompositor
+{
+public:
+	proxy_IVRCompositor(IVRCompositor* real) { realImpl = real; }
+
+	THISCALL void SetTrackingSpace( ETrackingUniverseOrigin eOrigin )
+  {
+    realImpl->SetTrackingSpace(eOrigin);
+    return;
+  }
+
+	THISCALL ETrackingUniverseOrigin GetTrackingSpace()
+  {
+    return realImpl->GetTrackingSpace();
+  }
+
+	THISCALL EVRCompositorError WaitGetPoses( VR_ARRAY_COUNT(unRenderPoseArrayCount) TrackedDevicePose_t* pRenderPoseArray, uint32_t unRenderPoseArrayCount,
+		VR_ARRAY_COUNT(unGamePoseArrayCount) TrackedDevicePose_t* pGamePoseArray, uint32_t unGamePoseArrayCount )
+  {
+    return realImpl->WaitGetPoses(pRenderPoseArray, unRenderPoseArrayCount, pGamePoseArray, unGamePoseArrayCount);
+  }
+
+	THISCALL EVRCompositorError GetLastPoses( VR_ARRAY_COUNT( unRenderPoseArrayCount ) TrackedDevicePose_t* pRenderPoseArray, uint32_t unRenderPoseArrayCount,
+		VR_ARRAY_COUNT( unGamePoseArrayCount ) TrackedDevicePose_t* pGamePoseArray, uint32_t unGamePoseArrayCount )
+  {
+    return realImpl->GetLastPoses(pRenderPoseArray, unRenderPoseArrayCount, pGamePoseArray, unGamePoseArrayCount);
+  }
+
+	THISCALL EVRCompositorError GetLastPoseForTrackedDeviceIndex( TrackedDeviceIndex_t unDeviceIndex, TrackedDevicePose_t *pOutputPose, TrackedDevicePose_t *pOutputGamePose )
+  {
+    return realImpl->GetLastPoseForTrackedDeviceIndex(unDeviceIndex, pOutputPose, pOutputGamePose);
+  }
+
+	THISCALL EVRCompositorError Submit( EVREye eEye, const Texture_t *pTexture, const VRTextureBounds_t* pBounds, EVRSubmitFlags nSubmitFlags )
+  {
+    return realImpl->Submit(eEye, pTexture, pBounds, nSubmitFlags);
+  }
+
+	THISCALL void ClearLastSubmittedFrame()
+  {
+    realImpl->ClearLastSubmittedFrame();
+    return;
+  }
+
+	THISCALL void PostPresentHandoff()
+  {
+    realImpl->PostPresentHandoff();
+    return;
+  }
+
+	THISCALL bool GetFrameTiming( Compositor_FrameTiming *pTiming, uint32_t unFramesAgo )
+  {
+    return realImpl->GetFrameTiming(pTiming, unFramesAgo);
+  }
+
+	THISCALL uint32_t GetFrameTimings( Compositor_FrameTiming *pTiming, uint32_t nFrames )
+  {
+    return realImpl->GetFrameTimings(pTiming, nFrames);
+  }
+
+	THISCALL float GetFrameTimeRemaining()
+  {
+    return realImpl->GetFrameTimeRemaining();
+  }
+
+	THISCALL void GetCumulativeStats( Compositor_CumulativeStats *pStats, uint32_t nStatsSizeInBytes )
+  {
+    realImpl->GetCumulativeStats(pStats, nStatsSizeInBytes);
+    return;
+  }
+
+	THISCALL void FadeToColor( float fSeconds, float fRed, float fGreen, float fBlue, float fAlpha, bool bBackground )
+  {
+    realImpl->FadeToColor(fSeconds, fRed, fGreen, fBlue, fAlpha, bBackground);
+    return;
+  }
+
+	THISCALL void GetCurrentFadeColor( HmdColor_t* ret, bool bBackground )
+  {
+    // ERP hack
+    *ret = realImpl->GetCurrentFadeColor(bBackground);
+    return;
+  }
+
+	THISCALL void FadeGrid( float fSeconds, bool bFadeIn )
+  {
+    realImpl->FadeGrid(fSeconds, bFadeIn);
+    return;
+  }
+
+	THISCALL float GetCurrentGridAlpha()
+  {
+    return realImpl->GetCurrentGridAlpha();
+  }
+
+	THISCALL EVRCompositorError SetSkyboxOverride( VR_ARRAY_COUNT( unTextureCount ) const Texture_t *pTextures, uint32_t unTextureCount )
+  {
+    return realImpl->SetSkyboxOverride(pTextures, unTextureCount);
+  }
+
+	THISCALL void ClearSkyboxOverride()
+  {
+    realImpl->ClearSkyboxOverride();
+    return;
+  }
+
+	THISCALL void CompositorBringToFront()
+  {
+    realImpl->CompositorBringToFront();
+    return;
+  }
+
+	THISCALL void CompositorGoToBack()
+  {
+    realImpl->CompositorGoToBack();
+    return;
+  }
+
+	THISCALL void CompositorQuit()
+  {
+    realImpl->CompositorQuit();
+    return;
+  }
+
+	THISCALL bool IsFullscreen()
+  {
+    return realImpl->IsFullscreen();
+  }
+
+	THISCALL uint32_t GetCurrentSceneFocusProcess()
+  {
+    return realImpl->GetCurrentSceneFocusProcess();
+  }
+
+	THISCALL uint32_t GetLastFrameRenderer()
+  {
+    return realImpl->GetLastFrameRenderer();
+  }
+
+	THISCALL bool CanRenderScene()
+  {
+    return realImpl->CanRenderScene();
+  }
+
+	THISCALL void ShowMirrorWindow()
+  {
+    realImpl->ShowMirrorWindow();
+    return;
+  }
+
+	THISCALL void HideMirrorWindow()
+  {
+    realImpl->HideMirrorWindow();
+    return;
+  }
+
+	THISCALL bool IsMirrorWindowVisible()
+  {
+    return realImpl->IsMirrorWindowVisible();
+  }
+
+	THISCALL void CompositorDumpImages()
+  {
+    realImpl->CompositorDumpImages();
+    return;
+  }
+
+	THISCALL bool ShouldAppRenderWithLowResources()
+  {
+    return realImpl->ShouldAppRenderWithLowResources();
+  }
+
+	THISCALL void ForceInterleavedReprojectionOn( bool bOverride )
+  {
+    realImpl->ForceInterleavedReprojectionOn(bOverride);
+    return;
+  }
+
+	THISCALL void ForceReconnectProcess()
+  {
+    realImpl->ForceReconnectProcess();
+    return;
+  }
+
+	THISCALL void SuspendRendering( bool bSuspend )
+  {
+    realImpl->SuspendRendering(bSuspend);
+    return;
+  }
+
+	THISCALL vr::EVRCompositorError GetMirrorTextureD3D11( vr::EVREye eEye, void *pD3D11DeviceOrResource, void **ppD3D11ShaderResourceView )
+  {
+    printf("WOVR fixme: GetMirrorTextureD3D11 stub!\n");
+    return VRCompositorError_RequestFailed;
+  }
+
+	THISCALL void ReleaseMirrorTextureD3D11( void *pD3D11ShaderResourceView )
+  {
+    printf("WOVR fixme: ReleaseMirrorTextureD3D11 stub!\n");
+    return;
+  }
+
+	THISCALL vr::EVRCompositorError GetMirrorTextureGL( vr::EVREye eEye, vr::glUInt_t *pglTextureId, vr::glSharedTextureHandle_t *pglSharedTextureHandle )
+  {
+    return realImpl->GetMirrorTextureGL(eEye, pglTextureId, pglSharedTextureHandle);
+  }
+
+	THISCALL bool ReleaseSharedGLTexture( vr::glUInt_t glTextureId, vr::glSharedTextureHandle_t glSharedTextureHandle )
+  {
+    return realImpl->ReleaseSharedGLTexture(glTextureId, glSharedTextureHandle);
+  }
+
+	THISCALL void LockGLSharedTextureForAccess( vr::glSharedTextureHandle_t glSharedTextureHandle )
+  {
+    realImpl->LockGLSharedTextureForAccess(glSharedTextureHandle);
+    return;
+  }
+
+	THISCALL void UnlockGLSharedTextureForAccess( vr::glSharedTextureHandle_t glSharedTextureHandle )
+  {
+    realImpl->UnlockGLSharedTextureForAccess(glSharedTextureHandle);
+    return;
+  }
+
+	THISCALL uint32_t GetVulkanInstanceExtensionsRequired( VR_OUT_STRING() char *pchValue, uint32_t unBufferSize )
+  {
+    return realImpl->GetVulkanInstanceExtensionsRequired(pchValue, unBufferSize);
+  }
+
+	THISCALL uint32_t GetVulkanDeviceExtensionsRequired( VkPhysicalDevice_T *pPhysicalDevice, VR_OUT_STRING() char *pchValue, uint32_t unBufferSize )
+  {
+    return realImpl->GetVulkanDeviceExtensionsRequired(pPhysicalDevice, pchValue, unBufferSize);
+  }
+
+private:
+	IVRCompositor* realImpl;
+};
+
+// Declared in proxy_getters.h
+vr::IVRCompositor* getIVRCompositorProxy (IVRCompositor* real)
+{
+  return (IVRCompositor*) new proxy_IVRCompositor(real);
+}
---
+++ src/ivrrendermodels_32.cpp
@@ -0,0 +1,173 @@
+#include <cstdio>
+
+// HACK: No idea where this should actually be defined
+#define nullptr 0x0
+#include "openvr.h"
+using namespace vr;
+
+#include "repacked_structs.h"
+
+#define THISCALL __attribute__((thiscall))
+
+class clone_IVRRenderModels
+{
+  THISCALL virtual EVRRenderModelError LoadRenderModel_Async( const char *pchRenderModelName, Repacked_RenderModel_t **ppRenderModel ) = 0; // Struct packing mismatch
+	THISCALL virtual void FreeRenderModel( Repacked_RenderModel_t *pRenderModel ) = 0; // Struct packing mismatch
+	THISCALL virtual EVRRenderModelError LoadTexture_Async( TextureID_t textureId, Repacked_RenderModel_TextureMap_t **ppTexture ) = 0; // Struct packing mismatch
+	THISCALL virtual void FreeTexture( Repacked_RenderModel_TextureMap_t *pTexture ) = 0; // Struct packing mismatch
+	THISCALL virtual EVRRenderModelError LoadTextureD3D11_Async( TextureID_t textureId, void *pD3D11Device, void **ppD3D11Texture2D ) = 0;
+	THISCALL virtual EVRRenderModelError LoadIntoTextureD3D11_Async( TextureID_t textureId, void *pDstTexture ) = 0;
+	THISCALL virtual void FreeTextureD3D11( void *pD3D11Texture2D ) = 0;
+	THISCALL virtual uint32_t GetRenderModelName( uint32_t unRenderModelIndex, VR_OUT_STRING() char *pchRenderModelName, uint32_t unRenderModelNameLen ) = 0;
+	THISCALL virtual uint32_t GetRenderModelCount() = 0;
+	THISCALL virtual uint32_t GetComponentCount( const char *pchRenderModelName ) = 0;
+	THISCALL virtual uint32_t GetComponentName( const char *pchRenderModelName, uint32_t unComponentIndex, VR_OUT_STRING( ) char *pchComponentName, uint32_t unComponentNameLen ) = 0;
+	THISCALL virtual uint64_t GetComponentButtonMask( const char *pchRenderModelName, const char *pchComponentName ) = 0;
+	THISCALL virtual uint32_t GetComponentRenderModelName( const char *pchRenderModelName, const char *pchComponentName, VR_OUT_STRING( ) char *pchComponentRenderModelName, uint32_t unComponentRenderModelNameLen ) = 0;
+	THISCALL virtual bool GetComponentState( const char *pchRenderModelName, const char *pchComponentName, const vr::VRControllerState_t *pControllerState, const RenderModel_ControllerMode_State_t *pState, RenderModel_ComponentState_t *pComponentState ) = 0;
+	THISCALL virtual bool RenderModelHasComponent( const char *pchRenderModelName, const char *pchComponentName ) = 0;
+	THISCALL virtual uint32_t GetRenderModelThumbnailURL( const char *pchRenderModelName, VR_OUT_STRING() char *pchThumbnailURL, uint32_t unThumbnailURLLen, vr::EVRRenderModelError *peError ) = 0;
+	THISCALL virtual uint32_t GetRenderModelOriginalPath( const char *pchRenderModelName, VR_OUT_STRING() char *pchOriginalPath, uint32_t unOriginalPathLen, vr::EVRRenderModelError *peError ) = 0;
+	THISCALL virtual const char *GetRenderModelErrorNameFromEnum( vr::EVRRenderModelError error ) = 0;
+};
+
+class proxy_IVRRenderModels
+{
+public:
+  proxy_IVRRenderModels(IVRRenderModels* real) { realImpl = real; }
+
+  THISCALL EVRRenderModelError LoadRenderModel_Async( const char *pchRenderModelName, Repacked_RenderModel_t **ppRenderModel )
+  {
+    // Struct packing mismatch
+    // Because The API impl itself allocates the RM, things are a little complicated.
+    EVRRenderModelError ret;
+    RenderModel_t* linpackedRM;
+
+    ret = realImpl->LoadRenderModel_Async(pchRenderModelName, &linpackedRM);
+
+    // This doesn't appear to be spelled out in the API documentation, but hellovr_opengl suggests that the RM isn't
+    // allocated on error (including Loading).
+    if(ret == VRRenderModelError_None)
+    {
+      *ppRenderModel = new Repacked_RenderModel_t();
+      repackRenderModel(linpackedRM, *ppRenderModel);
+    }
+
+    return ret;
+  }
+
+	THISCALL void FreeRenderModel( Repacked_RenderModel_t *pRenderModel )
+  {
+    // Struct packing mismatch
+    realImpl->FreeRenderModel(pRenderModel->original);
+    delete pRenderModel;
+  }
+
+	THISCALL EVRRenderModelError LoadTexture_Async( TextureID_t textureId, Repacked_RenderModel_TextureMap_t **ppTexture )
+  {
+    // Struct packing mismatch
+    // Because The API impl itself allocates the texture, things are a little complicated.
+    EVRRenderModelError ret;
+    RenderModel_TextureMap_t* linpackedTex;
+
+    ret = realImpl->LoadTexture_Async(textureId, &linpackedTex);
+
+    // This doesn't appear to be spelled out in the API documentation, but hellovr_opengl suggests that the texture
+    // isn't allocated on error (including Loading).
+    if(ret == VRRenderModelError_None)
+    {
+      *ppTexture = new Repacked_RenderModel_TextureMap_t();
+      repackRenderModelTextureMap(linpackedTex, *ppTexture);
+    }
+
+    return ret;
+  }
+
+	THISCALL void FreeTexture( Repacked_RenderModel_TextureMap_t *pTexture )
+  {
+    // Struct packing mismatch
+    realImpl->FreeTexture(pTexture->original);
+    delete pTexture;
+  }
+
+	THISCALL EVRRenderModelError LoadTextureD3D11_Async( TextureID_t textureId, void *pD3D11Device, void **ppD3D11Texture2D )
+  {
+    printf("WOVR fixme: LoadTextureD3D11_Async stub!\n");
+    return VRRenderModelError_NotSupported;
+  }
+
+	THISCALL EVRRenderModelError LoadIntoTextureD3D11_Async( TextureID_t textureId, void *pDstTexture )
+  {
+    printf("WOVR fixme: LoadIntoTextureD3D11_Async stub!\n");
+    return VRRenderModelError_NotSupported;
+
+  }
+
+	THISCALL void FreeTextureD3D11( void *pD3D11Texture2D )
+  {
+    printf("WOVR fixme: FreeTextureD3D11 stub!\n");
+    return;
+  }
+
+	THISCALL uint32_t GetRenderModelName( uint32_t unRenderModelIndex, VR_OUT_STRING() char *pchRenderModelName, uint32_t unRenderModelNameLen )
+  {
+    return realImpl->GetRenderModelName(unRenderModelIndex, pchRenderModelName, unRenderModelNameLen);
+  }
+
+	THISCALL uint32_t GetRenderModelCount()
+  {
+    return realImpl->GetRenderModelCount();
+  }
+
+	THISCALL uint32_t GetComponentCount( const char *pchRenderModelName )
+  {
+    return realImpl->GetComponentCount(pchRenderModelName);
+  }
+
+	THISCALL uint32_t GetComponentName( const char *pchRenderModelName, uint32_t unComponentIndex, VR_OUT_STRING( ) char *pchComponentName, uint32_t unComponentNameLen )
+  {
+    return realImpl->GetComponentName(pchRenderModelName, unComponentIndex, pchComponentName, unComponentNameLen);
+  }
+
+	THISCALL uint64_t GetComponentButtonMask( const char *pchRenderModelName, const char *pchComponentName )
+  {
+    return realImpl->GetComponentButtonMask(pchRenderModelName, pchComponentName);
+  }
+
+	THISCALL uint32_t GetComponentRenderModelName( const char *pchRenderModelName, const char *pchComponentName, VR_OUT_STRING( ) char *pchComponentRenderModelName, uint32_t unComponentRenderModelNameLen )
+  {
+    return realImpl->GetComponentRenderModelName(pchRenderModelName, pchComponentName, pchComponentRenderModelName, unComponentRenderModelNameLen);
+  }
+
+	THISCALL bool GetComponentState( const char *pchRenderModelName, const char *pchComponentName, const vr::VRControllerState_t *pControllerState, const RenderModel_ControllerMode_State_t *pState, RenderModel_ComponentState_t *pComponentState )
+  {
+    return realImpl->GetComponentState(pchRenderModelName, pchComponentName, pControllerState, pState, pComponentState);
+  }
+
+	THISCALL bool RenderModelHasComponent( const char *pchRenderModelName, const char *pchComponentName )
+  {
+    return realImpl->RenderModelHasComponent(pchRenderModelName, pchComponentName);
+  }
+
+	THISCALL uint32_t GetRenderModelThumbnailURL( const char *pchRenderModelName, VR_OUT_STRING() char *pchThumbnailURL, uint32_t unThumbnailURLLen, vr::EVRRenderModelError *peError )
+  {
+    return realImpl->GetRenderModelThumbnailURL(pchRenderModelName, pchThumbnailURL, unThumbnailURLLen, peError);
+  }
+
+	THISCALL uint32_t GetRenderModelOriginalPath( const char *pchRenderModelName, VR_OUT_STRING() char *pchOriginalPath, uint32_t unOriginalPathLen, vr::EVRRenderModelError *peError )
+  {
+    return realImpl->GetRenderModelOriginalPath(pchRenderModelName, pchOriginalPath, unOriginalPathLen, peError);
+  }
+
+	THISCALL const char *GetRenderModelErrorNameFromEnum( vr::EVRRenderModelError error )
+  {
+    return realImpl->GetRenderModelErrorNameFromEnum(error);
+  }
+private:
+  IVRRenderModels* realImpl;
+};
+
+vr::IVRRenderModels* getIVRRenderModelsProxy (IVRRenderModels* real)
+{
+  return (IVRRenderModels*) new proxy_IVRRenderModels(real);
+}
