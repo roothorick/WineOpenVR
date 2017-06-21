09c014bf079a26cf714dee5ff841768bb9ddc826
Ben "root" Anderson <roothorick@gmail.com>
2017-06-21 16:30:39 -0500

    Tweak declarations of exported methods (squelch warning)

--- src/wovr_exports.cpp
+++ src/wovr_exports.cpp
@@ -3,7 +3,7 @@
 #include <cstring>
 
 /* Interface dispatcher. Try to acquire the native impl, then create and return our proxy. */
-VR_INTERFACE void *VR_CALLTYPE WOVR_GetGenericInterface(const char *pchInterfaceVersion, EVRInitError *peError)
+extern "C" void *VR_CALLTYPE WOVR_GetGenericInterface(const char *pchInterfaceVersion, EVRInitError *peError)
 {
     printf("WOVR info: App requested API version %s\n", pchInterfaceVersion);
 
@@ -61,49 +61,49 @@ VR_INTERFACE void *VR_CALLTYPE WOVR_GetGenericInterface(const char *pchInterface
     return (void*) 0x1;
 }
 
-VR_INTERFACE uint32_t VR_CALLTYPE WOVR_GetInitToken()
+extern "C" uint32_t VR_CALLTYPE WOVR_GetInitToken()
 {
   return VR_GetInitToken();
 }
 
-VR_INTERFACE const char *VR_CALLTYPE WOVR_GetVRInitErrorAsEnglishDescription(EVRInitError error)
+extern "C" const char *VR_CALLTYPE WOVR_GetVRInitErrorAsEnglishDescription(EVRInitError error)
 {
   return VR_GetVRInitErrorAsEnglishDescription(error);
 }
 
-VR_INTERFACE const char*VR_CALLTYPE WOVR_GetVRInitErrorAsSymbol(EVRInitError error)
+extern "C" const char *VR_CALLTYPE WOVR_GetVRInitErrorAsSymbol(EVRInitError error)
 {
   return VR_GetVRInitErrorAsSymbol(error);
 }
 
-VR_INTERFACE uint32_t VR_CALLTYPE WOVR_InitInternal(EVRInitError *peError, EVRApplicationType eApplicationType)
+extern "C" uint32_t VR_CALLTYPE WOVR_InitInternal(EVRInitError *peError, EVRApplicationType eApplicationType)
 {
   return VR_InitInternal(peError, eApplicationType);
 }
 
-VR_INTERFACE bool VR_CALLTYPE WOVR_IsHmdPresent()
+extern "C" bool VR_CALLTYPE WOVR_IsHmdPresent()
 {
   return VR_IsHmdPresent();
 }
 
-VR_INTERFACE bool VR_CALLTYPE WOVR_IsInterfaceVersionValid(const char *pchInterfaceVersion)
+extern "C" bool VR_CALLTYPE WOVR_IsInterfaceVersionValid(const char *pchInterfaceVersion)
 {
   printf("WOVR info: App requested version %s\n", pchInterfaceVersion);
   return VR_IsInterfaceVersionValid(pchInterfaceVersion);
 }
 
-VR_INTERFACE bool VR_CALLTYPE WOVR_IsRuntimeInstalled()
+extern "C" bool VR_CALLTYPE WOVR_IsRuntimeInstalled()
 {
   return VR_IsRuntimeInstalled();
 }
 
-VR_INTERFACE const char *VR_CALLTYPE WOVR_RuntimePath()
+extern "C" const char *VR_CALLTYPE WOVR_RuntimePath()
 {
   // FIXME: This should convert to a Wndows X:\\ type path before returning
   return VR_RuntimePath();
 }
 
-VR_INTERFACE void VR_CALLTYPE WOVR_ShutdownInternal()
+extern "C" void VR_CALLTYPE WOVR_ShutdownInternal()
 {
   VR_ShutdownInternal();
 }
