00bbd7def09eca4696c0afa412f167752d7025a1
Ben "root" Anderson <roothorick@gmail.com>
2017-06-22 15:06:52 -0500

    Missed add on ivrextendeddisplay_32.cpp for IVRExtendedDisplay proxy

---
+++ src/ivrextendeddisplay_32.cpp
@@ -0,0 +1,48 @@
+#include "common.h"
+
+/*
+This probably won't ever work correctly, but this part of the API isn't intended for production use anyway.
+It is only implemented in order to "complete" the API.
+*/
+
+class clone_IVRExtendedDisplay
+{
+public:
+	THISCALL virtual void GetWindowBounds( int32_t *pnX, int32_t *pnY, uint32_t *pnWidth, uint32_t *pnHeight ) = 0;
+	THISCALL virtual void GetEyeOutputViewport( EVREye eEye, uint32_t *pnX, uint32_t *pnY, uint32_t *pnWidth, uint32_t *pnHeight ) = 0;
+	THISCALL virtual void GetDXGIOutputInfo( int32_t *pnAdapterIndex, int32_t *pnAdapterOutputIndex ) = 0;
+};
+
+class proxy_IVRExtendedDisplay : public clone_IVRExtendedDisplay
+{
+public:
+	proxy_IVRExtendedDisplay(IVRExtendedDisplay* real) { realImpl = real; }
+
+	THISCALL void GetWindowBounds( int32_t *pnX, int32_t *pnY, uint32_t *pnWidth, uint32_t *pnHeight )
+	{
+		realImpl->GetWindowBounds(pnX, pnY, pnWidth, pnHeight);
+		return;
+	}
+
+	THISCALL void GetEyeOutputViewport( EVREye eEye, uint32_t *pnX, uint32_t *pnY, uint32_t *pnWidth, uint32_t *pnHeight )
+	{
+		realImpl->GetEyeOutputViewport(eEye, pnX, pnY, pnWidth, pnHeight);
+		return;
+	}
+
+	THISCALL void GetDXGIOutputInfo( int32_t *pnAdapterIndex, int32_t *pnAdapterOutputIndex )
+	{
+		printf("WOVR fixme: IVRExtendedDisplay::GetDXGIOutputInfo stub!\n");
+		*pnAdapterIndex = -1;
+		*pnAdapterOutputIndex = -1;
+		return;
+	}
+
+private:
+	IVRExtendedDisplay* realImpl;
+};
+
+IVRExtendedDisplay* getIVRExtendedDisplayProxy(IVRExtendedDisplay* real)
+{
+	return (IVRExtendedDisplay*) new proxy_IVRExtendedDisplay(real);
+}
