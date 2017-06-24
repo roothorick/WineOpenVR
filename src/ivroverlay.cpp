6ed7dc7ef54c648e0a860cd6f712ffdc574a8c4e
Ben "root" Anderson <roothorick@gmail.com>
2017-06-24 17:50:02 -0500

    Remove redundant ivroverlay with only comments

--- src/ivroverlay.cpp
+++
@@ -1,7 +0,0 @@
-// WARNING: These structs do not appear to have an explicitly defined packing
-// alignment:
-// * VROverlayIntersectionParams_t
-// * VROverlayIntersectionResults_t
-// * IntersectionMaskRectangle_t
-// * IntersectionMaskCircle_t
-// It's not known at this time whether they differ Windows<->Linux
