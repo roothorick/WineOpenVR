8b7b69ad99b2a227dfb42ec0240883d4f3354224
roothorick <roothorick@gmail.com>
2017-06-27 18:30:48 -0500

    Fix implied ms_abi where it shouldn't be

--- src/common.h
+++ src/common.h
@@ -4,9 +4,10 @@
 
 // HACK: No idea where this should actually be defined
 #define nullptr 0x0
-#pragma GCC diagnostic ignored "-Wattributes"
+// If _WIN32 is defined, openvr.h will decorate the exported functions with __declspec(dllimport) which implies
+// __attribute__((ms_abi)). This makes things explode, as we need to call them with SysV ABI.
+#undef _WIN32
 #include "openvr.h"
-#pragma GCC diagnostic warning "-Wattributes"
 using namespace vr;
 
 #ifdef __x86_64__
