2199f38b83d97acdb8f2758ddc4ede8973d8d97b
roothorick <roothorick@gmail.com>
2017-07-09 07:39:59 -0500

    Changes in IVRSystem_016

--- src/ivrsystem/ivrsystem.h
+++ src/ivrsystem/ivrsystem.h
@@ -27,7 +27,9 @@ public:
 	WOVR_ENTRY virtual bool GetTimeSinceLastVsync( float *pfSecondsSinceLastVsync, uint64_t *pulFrameCounter ) = 0;
 	WOVR_ENTRY virtual int32_t GetD3D9AdapterIndex() = 0;
 	WOVR_ENTRY virtual void GetDXGIOutputInfo( int32_t *pnAdapterIndex ) = 0;
+#if ABIVER <= 016
 	WOVR_ENTRY virtual void GetOutputDevice( uint64_t *pnDevice, ETextureType textureType ) = 0;
+#endif
 	WOVR_ENTRY virtual bool IsDisplayOnDesktop() = 0;
 	WOVR_ENTRY virtual bool SetDisplayVisibility( bool bIsVisibleOnDesktop ) = 0;
 	WOVR_ENTRY virtual void GetDeviceToAbsoluteTrackingPose( ETrackingUniverseOrigin eOrigin, float fPredictedSecondsToPhotonsFromNow, VR_ARRAY_COUNT(unTrackedDevicePoseArrayCount) TrackedDevicePose_t *pTrackedDevicePoseArray, uint32_t unTrackedDevicePoseArrayCount ) = 0;
@@ -115,6 +117,7 @@ public:
 		return;
 	}
 
+#if ABIVER <= 016
 	WOVR_ENTRY void GetOutputDevice( uint64_t *pnDevice, ETextureType textureType )
 	{
 		if(textureType == TextureType_DirectX || textureType == TextureType_DirectX12)
@@ -123,6 +126,7 @@ public:
 			VRSystem()->GetOutputDevice(pnDevice, textureType);
 		return;
 	}
+#endif
 
 	WOVR_ENTRY bool IsDisplayOnDesktop()
 	{
